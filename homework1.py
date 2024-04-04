173bde6573dc52d2c3bb4f28dc74b53769235ae4
navinksrk <140748337+navinksrk@users.noreply.github.com>
2024-04-04 16:44:33 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -16,11 +16,6 @@
 # "The value at position __ is __" for every element in the loop, where the first blank is the
 # index location and the second blank the object at that index location.
 
-list_of_objects = [0,1,2,3]
-i = 0
-for item in list_of_objects:
-    print(f"The value at position {i} is {item}")
-    i += 1
 
 # Question 1.2: A palindrome is a word or phrase that is the same both forwards and backwards. Write
 # code that takes a variable of any string, then tests to see whether it qualifies as a palindrome.
